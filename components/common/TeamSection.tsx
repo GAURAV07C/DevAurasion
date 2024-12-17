73eccd0197b037f9551115c6838284b52df75caa
GAURAV07C <gaurav989267@gmail.com>
2024-12-18 04:03:28 +0530

    card fixed

--- components/common/TeamSection.tsx
+++ components/common/TeamSection.tsx
@@ -50,62 +50,66 @@ const TeamSection: React.FC<TeamSectionProps> = ({
             <Card
               key={index}
               className="relative w-[300px] h-[300px] mx-auto border-2 overflow-hidden group"
-              style={
-                {
-                  backgroundImage: `url('${person.backgroundImage}')`,
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                } as React.CSSProperties
-              }
+              style={{
+                backgroundImage: `url('${person.backgroundImage}')`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              } as React.CSSProperties}
             >
               {/* Overlay */}
               <div className="absolute inset-0 opacity-40 group-hover:opacity-60 transition-opacity duration-300"></div>
 
               {/* Card Content */}
-              <CardContent className="absolute inset-0 flex flex-col justify-end p-6 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <CardHeader className="text-start">
-                  <CardTitle className="text-lg sm:text-xl lg:text-2xl font-bold text-[#5D5A88]">
-                    {person.name}
-                  </CardTitle>
-                  <CardDescription className="text-sm sm:text-base lg:text-lg font-bold text-[#8D8BA7]">
-                    {person.role}
-                  </CardDescription>
-                </CardHeader>
-                <CardFooter className="flex gap-4 mt-2 text-[#8D8BA7]">
-                  {person.links?.facebook && (
-                    <Link href={person.links.facebook} passHref>
-                      <FaFacebook
-                        size={24}
-                        className="cursor-pointer hover:text-blue-500"
-                      />
-                    </Link>
-                  )}
-                  {person.links?.twitter && (
-                    <Link href={person.links.twitter} passHref>
-                      <FaTwitter
-                        size={24}
-                        className="cursor-pointer hover:text-blue-400"
-                      />
-                    </Link>
-                  )}
-                  {person.links?.instagram && (
-                    <Link href={person.links.instagram} passHref>
-                      <FaInstagram
-                        size={24}
-                        className="cursor-pointer hover:text-pink-500"
-                      />
-                    </Link>
-                  )}
-                  {person.links?.linkedin && (
-                    <Link href={person.links.linkedin} passHref>
-                      <FaLinkedin
-                        size={24}
-                        className="cursor-pointer hover:text-blue-600"
-                      />
-                    </Link>
-                  )}
-                </CardFooter>
+              <CardContent className="absolute inset-0 flex flex-col justify-end text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="">
+                  <CardHeader className="text-start -mt-1">
+                    <CardTitle className="text-lg sm:text-xl lg:text-2xl font-bold text-[#5D5A88] -mt-8">
+                      {person.name}
+                    </CardTitle>
+
+                    {/* Reduced the gap between name and role */}
+                    <CardDescription className="text-sm sm:text-base lg:text-lg font-bold text-[#8D8BA7] mt-2">
+                      {person.role}
+                    </CardDescription>
+                  </CardHeader>
+
+                  {/* Social Media Links */}
+                  <CardFooter className="flex gap-4 -mt-6 text-[#8D8BA7]">
+                    {person.links?.facebook && (
+                      <Link href={person.links.facebook} passHref>
+                        <FaFacebook
+                          size={24}
+                          className="cursor-pointer hover:text-blue-500"
+                        />
+                      </Link>
+                    )}
+                    {person.links?.twitter && (
+                      <Link href={person.links.twitter} passHref>
+                        <FaTwitter
+                          size={24}
+                          className="cursor-pointer hover:text-blue-400"
+                        />
+                      </Link>
+                    )}
+                    {person.links?.instagram && (
+                      <Link href={person.links.instagram} passHref>
+                        <FaInstagram
+                          size={24}
+                          className="cursor-pointer hover:text-pink-500"
+                        />
+                      </Link>
+                    )}
+                    {person.links?.linkedin && (
+                      <Link href={person.links.linkedin} passHref>
+                        <FaLinkedin
+                          size={24}
+                          className="cursor-pointer hover:text-blue-600"
+                        />
+                      </Link>
+                    )}
+                  </CardFooter>
+                </div>
               </CardContent>
             </Card>
           ))}
